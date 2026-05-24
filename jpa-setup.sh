27de6e50c972df76ac891e5b9653dd5365f595c4
Yeuemnhungkhongvoitoi <nnajpa001@gmail.com>
2026-05-24 22:11:12 +0700

    Update jpa-setup.sh

--- jpa-setup.sh
+++ jpa-setup.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "Đang cài đặt môi trường cho JPA Tool..."
+echo "Đang cài đặt môi trường cho Tester..."
 termux-setup-storage
 pkg update && pkg upgrade -y
 pkg install python git curl -y
@@ -9,8 +9,7 @@ pkg install python git curl -y
 pip install requests tqdm
 
 # Tải script chính từ link GitHub của bạn
-echo "Đang tải JPA Tool từ GitHub..."
-curl -o /sdcard/Download/jpa-download.py https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ToolDownload/refs/heads/main/jpa-download.py
+echo "Đang tải JPA Tester từ GitHub..."
+curl -o /sdcard/Download/tester.py https://raw.githubusercontent.com/Yeuemnhungkhongvoitoi/ToolDownload/refs/heads/main/tester.py
 
 echo "Cài đặt hoàn tất!"
-echo "Để chạy tool, hãy sử dụng lệnh: Run tool"
